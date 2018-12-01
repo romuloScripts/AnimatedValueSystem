1e3c30b2e7e3d53da813326e51b71f8f7fb36eff
Romulo San <romulossan@gmail.com>
2018-12-01 21:12:52 -0200

    ok

---
+++ LocalOffsetValueEaseCurve.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalOffsetValueEaseCurve : ObjectValue {
+
+	public float amplitude=1;
+	public EasingCurve curve;
+	public Vector3 localPos,localPos2;
+	public Vector3 posIni;
+	public bool ResetPosIni;
+
+	void Awake()
+	{
+		if (ResetPosIni)
+			SetPosIni();
+		transform.localPosition = localPos;
+	}
+
+	[Button("Set Pos Ini")]
+	public void SetPosIni()
+	{
+		posIni = transform.localPosition;
+	}
+
+	public void SetPosIni(Vector3 pos)
+	{
+		posIni = pos;
+	}
+
+	public override void setValue (float value)
+	{
+		transform.localPosition = posIni + Vector3.LerpUnclamped (localPos, localPos2, curve.Evaluate (value))*amplitude;
+	}
+
+	public void SetAmplitude(float n) 
+	{
+		amplitude = n;
+	}
+}
